348a5dccd015fd597970a80cf1330905c8541eea
Javier Salazar <17890441+jjaves@users.noreply.github.com>
2025-05-13 01:00:28 -0700

    flake8 hug

--- local_dev.py
+++ local_dev.py
@@ -3,9 +3,11 @@ from image_processor_func.main import process_image_for_transformation
 
 app = Flask(__name__)
 
+
 @app.route('/process_image', methods=['GET'])
 def process_image():
     return process_image_for_transformation(request)
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=8080)
