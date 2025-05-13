6162e46d87cd3e628949dfce1793ad83631ad33c
Javier Salazar <17890441+jjaves@users.noreply.github.com>
2025-05-13 13:29:33 -0700

    feat: include env file

--- local_dev.py
+++ local_dev.py
@@ -1,5 +1,17 @@
-from flask import Flask, request
+from dotenv import load_dotenv
+from os import path
+
+script_dir = path.abspath(path.dirname(__file__))
+dotenv_file_path = path.join(script_dir, '.env')
+
+if path.exists(dotenv_file_path):
+    load_dotenv(dotenv_path=dotenv_file_path)
+    print(f"local_dev.py: Loaded .env file from {dotenv_file_path}")
+else:
+    print(f"local_dev.py: .env file not found at {dotenv_file_path}. ")
+
 from image_processor_func.main import process_image_for_transformation
+from flask import Flask, request
 
 app = Flask(__name__)
 
